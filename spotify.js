ee56adabf31acf55b6197d0346513d705486acbf
stephtzhang <stephtzhang@gmail.com>
2015-01-24 17:16:10 -0500

    Move makeGainNode and makeOscillator to Theremin class

--- spotify.js
+++ spotify.js
@@ -1,18 +1,7 @@
 $(document).ready(function() {
-	audioContext = new AudioContext();
-
 	var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
 
-	var oscillator = audioCtx.createOscillator();
-	var gainNode = audioCtx.createGain();
-
-	oscillator.connect(gainNode);
-	gainNode.connect(audioCtx.destination);
-
-	oscillator.type = 'square';
-	oscillator.detune.value = 10 // value in cents
-	oscillator.start();
-	theremin = new Theremin(oscillator, gainNode);
+	theremin = new Theremin(audioCtx);
 
 	$(document).mousemove(function(event) {
 		$(".x").text("X: " + event.pageX);
