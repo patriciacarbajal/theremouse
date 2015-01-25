6ded2212f2fd98de92cfa8c13860f214572f698a
stephtzhang <stephtzhang@gmail.com>
2015-01-24 19:42:18 -0500

    Abstract out findAndUpdate from child_added listener

--- spotify.js
+++ spotify.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
-  	var myDataRef = new Firebase('https://theremouse.firebaseio.com/');
-  	myDataRef.remove()
+  var myDataRef = new Firebase('https://theremouse.firebaseio.com/');
 	var theremins = {};
 	var userId = Math.floor(Math.random() * 1000000) + 1;
 
-	var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-
+	audioCtx = new(window.AudioContext || window.webkitAudioContext)();
 
 	$(document).mousemove(function(event) {
 		$(".x").text("X: " + event.pageX);
 		$(".y").text("Y: " + event.pageY);
 
 		myDataRef.push({x: event.pageX, y: event.pageY, userId: userId});
-		myDataRef.on('child_added', function(snapshot) {
-			if (!theremins[userId]) {
-				var theremin = new Theremin(audioCtx);
-				theremins[userId] = theremin;
-			} 
-			var x = snapshot.val().x;
-			var y = snapshot.val().y;
-			theremins[userId].update(x, y);
-
-		});
-
 	});
 
+  myDataRef.on('child_added', function(snapshot) {
+    var x = snapshot.val().x;
+    var y = snapshot.val().y;
+    findAndUpdate(x, y);
+  });
+
+  var findAndUpdate = function(x, y) {
+    if (!theremins[userId]) {
+      theremins[userId] = new Theremin(audioCtx);
+    }
+    theremins[userId].update(x, y);
+  }
 });
+
 
