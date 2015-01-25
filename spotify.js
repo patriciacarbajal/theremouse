2d9b28fb205823dab55ca16cfd3924ad2ef72af5
patriciacarbajal <carbajal.patricia@gmail.com>
2015-01-24 19:19:14 -0500

    jquery function updated

--- spotify.js
+++ spotify.js
@@ -21,28 +21,40 @@ $(document).ready(function() {
 		theremin.update(event.pageX, event.pageY);
 	});
 
-  $('.line1').hover(function() {
-      $('body').css('background-color', 'f8f087').fadeIn(3000);
-  })
-  $('.line2').hover(function() {
-    $("body").fadeIn('slow').css('background-color', 'b7e3c0');
-  })
-  $('.line3').hover(function() {
-    $("body").fadeIn('slow').css('background-color', 'ffc48c');
-  })
-  $('.line4').hover(function() {
-    $("body").fadeIn('slow').css('background-color', 'dbbae5');
-  })
- $('.line5').hover(function() {
-    $("body").fadeIn('slow').css('background-color', 'f39dd4');
-  })
-   $('.line6').hover(function() {
-    $("body").fadeIn('slow').css('background-color', 'eecaf5');
-  })
-     $('.line7').hover(function() {
-    $("body").fadeIn('slow').css('background-color', '95fefd');
-  })
+ //  $('.line1').hover(function() {
+ //      $('body').css('background-color', 'f8f087').fadeIn(3000);
+ //  })
+ //  $('.line2').hover(function() {
+ //    $("body").fadeIn('slow').css('background-color', 'b7e3c0');
+ //  })
+ //  $('.line3').hover(function() {
+ //    $("body").fadeIn('slow').css('background-color', 'ffc48c');
+ //  })
+ //  $('.line4').hover(function() {
+ //    $("body").fadeIn('slow').css('background-color', 'dbbae5');
+ //  })
+ // $('.line5').hover(function() {
+ //    $("body").fadeIn('slow').css('background-color', 'f39dd4');
+ //  })
+ //   $('.line6').hover(function() {
+ //    $("body").fadeIn('slow').css('background-color', 'eecaf5');
+ //  })
+ //     $('.line7').hover(function() {
+ //    $("body").fadeIn('slow').css('background-color', '95fefd');
+ //  })
       
+  $('body').mousemove(function(event) {
+    var width = $('body').width(),
+     height = $('body').height(),
+     axisX = event.pageX,
+     axisY = event.pageY;
+ 
+    var hue = Math.floor(axisX / width * 360),
+     saturation = Math.floor(axisY / height * 100),
+     lightness = Math.floor(axisY / height * 100);
+ 
+    $('body').css('background', 'hsl(' + hue + ',' + saturation + '%, ' + lightness + '%)');
+  });
 
 
 });
